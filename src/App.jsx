1465943e7f2c17e7869da09934d47d243c3ecc92
knowsarzehmeh <osarzeh@gmail.com>
2025-06-15 10:24:54 +0100

    add comment

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import FloatingImage from "./components/Story";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
+// Inspired by: https://github.com/adrianhajdin/award-winning-website
+
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
